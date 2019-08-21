6c1080a9ad2e75b1b09af6b784460a0d595258b0
Paul Martin <mussym@live.com>
2019-08-21 14:38:16 -0500

    adding menus

--- src/main/scala/MainWindow.scala
+++ src/main/scala/MainWindow.scala
@@ -2,10 +2,33 @@ package com.parinherm.scalaswing.demo
 
 import scala.swing._
 import scala.swing.event.ButtonClicked
+import scala.swing.event.Key
+import javax.swing.{Box, BoxLayout, ImageIcon, JDialog, JFrame, UIManager}
+import java.awt.{BorderLayout, Color, Component, Dimension, Graphics, Image, Point, Toolkit}
+import java.awt.event._
+import java.awt.event.KeyEvent.VK_B
+import java.awt.image.BufferedImage
+import java.net.URL
+import javax.swing.KeyStroke
 
 
 class AppFrame extends MainFrame() {
-  println("Mainframe")
+
+  title = "FrameDemo2"
+
+
+  //Use the Java look and feel.  This needs to be done before the frame is created
+  //so the companion object FrameDemo2 cannot simply extend SimpleSwingApplcation.
+//  try {
+//    UIManager.setLookAndFeel(
+//      UIManager.getCrossPlatformLookAndFeelClassName());
+//  } catch {
+//    case e: Exception => ;
+//  }
+  //Make sure we have nice window decorations.
+  JFrame.setDefaultLookAndFeelDecorated(true);
+  JDialog.setDefaultLookAndFeelDecorated(true);
+
   title = "fred"
   maximize()
 
@@ -17,8 +40,20 @@ class AppFrame extends MainFrame() {
   def createMenuBar(): MenuBar = {
     val mb = new MenuBar()
     val fileMenu = new Menu("File")
-    val fileMenuQuit = new MenuItem("Quit")
-    fileMenuQuit.mnemonic = Key.Val
+    fileMenu.mnemonic = Key.F
+
+    //val ctrlXKeyStroke = KeyStroke.getKeyStroke("control X")
+
+    //here is how to do menu with accelerator key
+    val fileMenuQuit = new MenuItem(new Action("Quit") {
+      accelerator = Some(KeyStroke.getKeyStroke("ctrl Q"))
+      def apply(): Unit =
+      {
+        MainWindow.quit()
+      }
+    })
+    
+
     fileMenu.contents += fileMenuQuit
     mb.contents += fileMenu
     mb.contents += new Menu("Edit")
