b54dd071812cd51d25fba138e0dde4393a8e76c6
Paul Martin <mussym@live.com>
2019-08-23 12:18:56 -0500

    dialog

--- src/main/scala/BookDialog.scala
+++ src/main/scala/BookDialog.scala
@@ -11,20 +11,31 @@ import net.miginfocom.swing._
 
 class BookDialog (owner: Window) extends Dialog (owner) {
   modal = true
-  //val panel = new BoxPanel(Orientation.Vertical)
-  val panel = new GridPanel(2, 2)
+  val panel = new BoxPanel(Orientation.Vertical)
+
 
   val label = new Label()
   label.text = "fred"
 
+  val txtName = new TextField()
+
   val btnOk = new Button("OK")
   val btnCancel = new Button("Cancel")
 
+
+
   panel.contents += label
+  panel.peer.add( txtName.peer, "growx")
   panel.contents += btnOk
   panel.contents += btnCancel
 
+
   defaultButton = btnOk
+
+  val layout = new MigLayout("wrap 2", "[shrink] [grow]", "[grow] [grow]")
+
+  panel.peer.setLayout(layout)
+  //this.peer.getContentPane().setLayout(layout)
   this.contents = panel
   minimumSize = new Dimension(640, 480)
 
